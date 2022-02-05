44d817681b6f47f3014605ae5a0312ef46d78eff
OlegHolovko <wodan78@gmail.com>
2022-02-05 23:39:00 +0200

    Init app

---
+++ haircutservie/src/main/java/com/holovko/haircutservie/HaircutserviceApplication.java
@@ -0,0 +1,13 @@
+package com.holovko.haircutservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaircutserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaircutserviceApplication.class, args);
+	}
+
+}
---
+++ haircutservie/src/test/java/com/holovko/haircutservie/HaircutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holovko.haircutservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaircutserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
