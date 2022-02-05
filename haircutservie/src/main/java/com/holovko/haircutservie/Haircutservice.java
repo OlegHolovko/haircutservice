b9bfb158dc0b1500d24c260ec671455c6aeb05b0
OlegHolovko <wodan78@gmail.com>
2022-02-06 00:43:12 +0200

    Main class rename

--- haircutservie/src/main/java/com/holovko/haircutservie/Haircutservice.java
+++ haircutservie/src/main/java/com/holovko/haircutservie/Haircutservice.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaircutserviceApplication {
+public class Haircutservice {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HaircutserviceApplication.class, args);
+
+		SpringApplication.run(Haircutservice.class, args);
 	}
 
 }
