1a39c88419872a6fa95245fce76573c8a26e09f2
OlegHolovko <wodan78@gmail.com>
2022-02-13 08:02:28 +0200

    Add company endpoints

--- src/main/java/com/holovko/haircutservie/stuff/web/CompanyController.java
+++ src/main/java/com/holovko/haircutservie/stuff/web/CompanyController.java
@@ -1,8 +1,6 @@
 package com.holovko.haircutservie.stuff.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/companies")
@@ -10,5 +8,25 @@ public class CompanyController {
     @GetMapping(value = "")
     public String list() {
         return "List of companies";
+    }
+
+    @GetMapping(value = "/{companyId}")
+    public String getById(@PathVariable(value = "companyId") Long companyId) {
+        return "Company data";
+    }
+
+    @PostMapping(value = "", produces = "application/json")
+    public String create(@RequestBody Object companyDTO) {
+        return "Company crated";
+    }
+
+    @PutMapping(value = "/{companyId}", produces = "application/json")
+    public String update(@PathVariable(value = "companyId") Long companyId, @RequestBody Object companyDTO) {
+        return "Company updated";
+    }
+
+    @DeleteMapping(value = "/{companyId}")
+    public String delete(@PathVariable(value = "companyId") Long companyId) {
+        return "Company deleted";
     }
 }
