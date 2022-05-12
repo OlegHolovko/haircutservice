18b844ae1b8700af9f1b6d09e872f02da7674e94
OlegHolovko <wodan78@gmail.com>
2022-05-12 18:58:23 +0300

    Add BaseEntity

---
+++ src/main/java/com/holovko/haircutservie/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.holovko.haircutservie.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseEntity {
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @JsonIgnore
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    @JsonIgnore
+    private LocalDateTime updatedAt;
+
+}
