925ebeaa579fbe8bf8f8489ba6ece573c2fd2f69
OlegHolovko <wodan78@gmail.com>
2022-05-14 12:39:41 +0300

    Add fields to Location model

--- src/main/java/com/holovko/haircutservie/domain/Location.java
+++ src/main/java/com/holovko/haircutservie/domain/Location.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "locations")
 @Getter
@@ -13,4 +13,29 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Location extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="country")
+    private String country;
+
+    @Column(name="region")
+    private String region;
+
+    @Column(name="city")
+    private String city;
+
+    @Column(name="address")
+    private String adress;
+
+    @Column(name="latitude")
+    private String latitude;
+
+    @Column(name="longitude")
+    private String longitude;
+
+    @OneToOne(mappedBy = "location")
+    private Company company;
 }
