2f81101dacf0901ac5b736f6217ba972c2644965
OlegHolovko <wodan78@gmail.com>
2022-05-14 13:39:59 +0300

    Add fields to OrderRequest model

--- src/main/java/com/holovko/haircutservie/domain/OrderRequest.java
+++ src/main/java/com/holovko/haircutservie/domain/OrderRequest.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "order_requests")
 @Getter
@@ -13,4 +14,25 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequest extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @NotNull
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone")
+    private String phone;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "schedule_id", referencedColumnName = "id")
+    private Schedule schedule;
+
+    @Column(name="is_processed")
+    private Boolean isProcessed;
 }
