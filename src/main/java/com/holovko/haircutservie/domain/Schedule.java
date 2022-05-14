5688764869611704901dc45a2e8bb227c80b07f0
OlegHolovko <wodan78@gmail.com>
2022-05-14 13:50:33 +0300

    Add fields to Schedule model

--- src/main/java/com/holovko/haircutservie/domain/Schedule.java
+++ src/main/java/com/holovko/haircutservie/domain/Schedule.java
@@ -1,11 +1,13 @@
 package com.holovko.haircutservie.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Table(name = "schedules")
 @Getter
@@ -13,4 +15,26 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Schedule extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "company_id", referencedColumnName = "id")
+    private Company company;
+
+    @Column(name="start_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @Column(name="end_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDate;
+
+    @Column(name="duration")
+    private Integer duration;
+
+    @Column(name="enabled")
+    private Boolean enabled;
 }
