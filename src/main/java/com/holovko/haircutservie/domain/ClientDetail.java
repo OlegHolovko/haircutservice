724f28f67c4c5dcf98fb72a60ad33c33e3d05a1d
OlegHolovko <wodan78@gmail.com>
2022-05-14 13:31:02 +0300

    Add fields to ClientDetail model

--- src/main/java/com/holovko/haircutservie/domain/ClientDetail.java
+++ src/main/java/com/holovko/haircutservie/domain/ClientDetail.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "client_details")
 @Getter
@@ -13,4 +14,22 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientDetail extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name="name")
+    private String name;
+
+    @NotNull
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone")
+    private String phone;
 }
