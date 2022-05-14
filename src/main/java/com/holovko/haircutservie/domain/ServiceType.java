1d72fa88c184b90a710c76b014276b2e177d4d95
OlegHolovko <wodan78@gmail.com>
2022-05-14 14:11:16 +0300

    Add fields to ServiceType model

--- src/main/java/com/holovko/haircutservie/domain/ServiceType.java
+++ src/main/java/com/holovko/haircutservie/domain/ServiceType.java
@@ -1,11 +1,14 @@
 package com.holovko.haircutservie.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Table(name = "service_types")
 @Getter
@@ -13,4 +16,28 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServiceType extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="price")
+    private BigDecimal price;
+
+    @Column(name="duration")
+    private Integer duration;
+
+    @Column(name="start_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @Column(name="end_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDate;
 }
