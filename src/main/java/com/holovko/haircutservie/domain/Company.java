9a5de24f3ac0b5e73c04a79100c25bc719d0d688
OlegHolovko <wodan78@gmail.com>
2022-05-14 11:26:26 +0300

    Add fields to Company model

--- src/main/java/com/holovko/haircutservie/domain/Company.java
+++ src/main/java/com/holovko/haircutservie/domain/Company.java
@@ -1,11 +1,15 @@
 package com.holovko.haircutservie.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Table(name = "companies")
 @Getter
@@ -13,5 +17,46 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Company extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
 
+    @NotNull
+    @Column(name="name")
+    private String name;
+
+    @Column(name="enabled")
+    private Boolean enabled;
+
+    @Column(name="filepath")
+    private String filepath;
+
+    @Column(name="logo")
+    private String logo;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="contacts")
+    private String contacts;
+
+    @Column(name="type")
+    private String type;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "location_id", referencedColumnName = "id")
+    private Location location;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company parent;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "parent_id")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<Company> children;
+
+    @JsonIgnore
+    public Set<Company> getChildren() {
+        return children;
+    }
 }
