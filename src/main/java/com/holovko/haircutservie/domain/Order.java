01b8609126f06baed1b62ec6664ae5944ed6ff37
OlegHolovko <wodan78@gmail.com>
2022-05-14 13:11:15 +0300

    Add fields to Order model

--- src/main/java/com/holovko/haircutservie/domain/Order.java
+++ src/main/java/com/holovko/haircutservie/domain/Order.java
@@ -1,11 +1,13 @@
 package com.holovko.haircutservie.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Table(name = "orders")
 @Getter
@@ -13,4 +15,50 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employee;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private ClientDetail clientDetail;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "request_id", referencedColumnName = "id")
+    private OrderRequest orderRequest;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    private ServiceType serviceType;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "schedule_id", referencedColumnName = "id")
+    private Schedule schedule;
+
+    @Column(name="start_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @Column(name="is_approved")
+    private Boolean isApproved;
+
+    @Column(name="approve_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime approveDate;
+
+    @Column(name="is_refused")
+    private Boolean isRefused;
+
+    @Column(name="refuse_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime refusetDate;
 }
