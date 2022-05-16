348754862712012e311b597c169aba2efd3cdafa
OlegHolovko <wodan78@gmail.com>
2022-05-16 13:21:34 +0300

    Move CompanyDTO

---
+++ src/main/java/com/holovko/haircutservie/module/company/web/dto/CompanyDTO.java
@@ -0,0 +1,13 @@
+package com.holovko.haircutservie.module.company.web.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyDTO {
+    private Long id;
+    private String name;
+}
