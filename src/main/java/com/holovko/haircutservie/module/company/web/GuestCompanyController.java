70fca4f4ce41173f6941bfdce86322786cd196cb
OlegHolovko <wodan78@gmail.com>
2022-05-17 07:16:22 +0300

    Rename CompanyController for using roles

--- src/main/java/com/holovko/haircutservie/module/company/web/GuestCompanyController.java
+++ src/main/java/com/holovko/haircutservie/module/company/web/GuestCompanyController.java
@@ -1,10 +1,18 @@
 package com.holovko.haircutservie.module.company.web;
 
+import com.holovko.haircutservie.module.company.service.CompanyService;
+import com.holovko.haircutservie.module.company.web.dto.GuestCompanyDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/companies")
-public class CompanyController {
+public class GuestCompanyController {
+    private final CompanyService companyService;
+
+    public GuestCompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
     @GetMapping("")
     public String list() {
         return "List of companies";
@@ -16,12 +24,12 @@ public class CompanyController {
     }
 
     @PostMapping(value = "", produces = "application/json")
-    public String create(@RequestBody CompanyDTO companyDTO) {
-        return "Company crated";
+    public String create(@RequestBody GuestCompanyDto companyDto) {
+        return companyService.create(companyDto);
     }
 
     @PutMapping(value = "/{companyId}", produces = "application/json")
-    public String update(@PathVariable Long companyId, @RequestBody CompanyDTO companyDTO) {
+    public String update(@PathVariable Long companyId, @RequestBody GuestCompanyDto companyDto) {
         return "Company updated";
     }
 
