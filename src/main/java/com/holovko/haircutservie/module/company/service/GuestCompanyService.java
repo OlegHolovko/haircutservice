0e986df7cf22084b22013a028e8ba8b8e6ab0c81
OlegHolovko <wodan78@gmail.com>
2022-05-20 15:50:37 +0300

    Implement GuestCompanyController and GuestCompanyService

--- src/main/java/com/holovko/haircutservie/module/company/service/GuestCompanyService.java
+++ src/main/java/com/holovko/haircutservie/module/company/service/GuestCompanyService.java
@@ -1,21 +1,10 @@
 package com.holovko.haircutservie.module.company.service;
 
-import com.holovko.haircutservie.domain.Company;
-import com.holovko.haircutservie.module.company.web.dto.GuestCompanyDto;
-import com.holovko.haircutservie.module.company.web.mapper.GuestCompanyMapper;
+import com.holovko.haircutservie.module.company.service.action.CompanyListable;
+import com.holovko.haircutservie.module.company.service.action.CompanyReadable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GuestCompanyService {
-    private final GuestCompanyMapper companyMapper;
+public class GuestCompanyService  implements CompanyListable, CompanyReadable {
 
-    public GuestCompanyService(GuestCompanyMapper companyMapper) {
-        this.companyMapper = companyMapper;
-    }
-
-
-    public String create(GuestCompanyDto companyDto) {
-        Company company = companyMapper.toCompany(companyDto);
-        return company.getName();
-    }
 }
--- src/main/java/com/holovko/haircutservie/module/company/web/GuestCompanyController.java
+++ src/main/java/com/holovko/haircutservie/module/company/web/GuestCompanyController.java
@@ -2,41 +2,35 @@ package com.holovko.haircutservie.module.company.web;
 
 import com.holovko.haircutservie.module.company.service.GuestCompanyService;
 import com.holovko.haircutservie.module.company.web.dto.GuestCompanyDto;
+import com.holovko.haircutservie.module.company.web.mapper.GuestCompanyMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/companies")
 public class GuestCompanyController {
     private final GuestCompanyService guestCompanyService;
+    private final GuestCompanyMapper guestCompanyMapper;
 
-    public GuestCompanyController(GuestCompanyService guestCompanyService) {
+    public GuestCompanyController(GuestCompanyService guestCompanyService, GuestCompanyMapper guestCompanyMapper) {
         this.guestCompanyService = guestCompanyService;
+        this.guestCompanyMapper = guestCompanyMapper;
     }
 
     @GetMapping("")
-    public String list() {
-        return "List of companies";
+    public List<GuestCompanyDto> list() {
+        return guestCompanyService.list()
+                .stream()
+                .map(guestCompanyMapper::fromCompany)
+                .toList();
     }
 
     @GetMapping("/{companyId}")
-    public String getById(@PathVariable Long companyId) {
-        return "Company data";
+    public GuestCompanyDto getById(@PathVariable Long companyId) {
+        return guestCompanyMapper.fromCompany(guestCompanyService.read(companyId));
     }
 
-    @PostMapping(value = "", produces = "application/json")
-    public String create(@RequestBody GuestCompanyDto companyDto) {
-        return guestCompanyService.create(companyDto);
-    }
-
-    @PutMapping(value = "/{companyId}", produces = "application/json")
-    public String update(@PathVariable Long companyId, @RequestBody GuestCompanyDto companyDto) {
-        return "Company updated";
-    }
-
-    @DeleteMapping("/{companyId}")
-    public String delete(@PathVariable Long companyId) {
-        return "Company deleted";
-    }
 }
 
 
