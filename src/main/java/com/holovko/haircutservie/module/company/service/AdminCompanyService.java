da7c3fe8eb9313c7f8f1aae46816a5b18fa4e92f
OlegHolovko <wodan78@gmail.com>
2022-05-21 11:00:04 +0300

    Add DI and Getter to services

--- src/main/java/com/holovko/haircutservie/module/company/service/AdminCompanyService.java
+++ src/main/java/com/holovko/haircutservie/module/company/service/AdminCompanyService.java
@@ -1,5 +1,6 @@
 package com.holovko.haircutservie.module.company.service;
 
+import com.holovko.haircutservie.module.company.repository.CompanyRepository;
 import com.holovko.haircutservie.module.company.service.action.CompanyListable;
 import com.holovko.haircutservie.module.company.service.action.CompanyReadable;
 
@@ -7,5 +8,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdminCompanyService  implements CompanyListable, CompanyReadable {
+    private final CompanyRepository companyRepository;
 
+    public AdminCompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public CompanyRepository getCompanyRepository() {
+        return companyRepository;
+    }
 }
--- src/main/java/com/holovko/haircutservie/module/company/service/UserCompanyService.java
+++ src/main/java/com/holovko/haircutservie/module/company/service/UserCompanyService.java
@@ -1,10 +1,19 @@
 package com.holovko.haircutservie.module.company.service;
 
+import com.holovko.haircutservie.module.company.repository.CompanyRepository;
 import com.holovko.haircutservie.module.company.service.action.CompanyListable;
 import com.holovko.haircutservie.module.company.service.action.CompanyReadable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserCompanyService implements CompanyListable, CompanyReadable {
+    private final CompanyRepository companyRepository;
 
+    public UserCompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public CompanyRepository getCompanyRepository() {
+        return companyRepository;
+    }
 }
