92c050178b7d320e87bee0c3680f22167ac096ed
OlegHolovko <wodan78@gmail.com>
2022-05-22 12:39:22 +0300

    Add sort type

--- src/main/java/com/holovko/haircutservie/domain/BaseEntity.java
+++ src/main/java/com/holovko/haircutservie/domain/BaseEntity.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.domain.Sort;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -15,6 +17,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
+    @Transient
+    public static final Sort SORT_BY_CREATED_AT_DESC =
+            Sort.by(Sort.Direction.DESC, "created_at");
 
     @Column(name = "created_at")
     @CreationTimestamp
