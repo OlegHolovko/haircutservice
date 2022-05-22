f8e5121ab1af2477cf1b62f636b503302f664e1b
OlegHolovko <wodan78@gmail.com>
2022-05-22 23:08:03 +0300

    Implement pagination and sorting

--- src/main/java/com/holovko/haircutservie/module/company/service/action/CompanyListable.java
+++ src/main/java/com/holovko/haircutservie/module/company/service/action/CompanyListable.java
@@ -2,12 +2,40 @@ package com.holovko.haircutservie.module.company.service.action;
 
 import com.holovko.haircutservie.domain.Company;
 import com.holovko.haircutservie.module.company.repository.CompanyRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import static com.holovko.haircutservie.domain.BaseEntity.SORT_BY_CREATED_AT_DESC;
-
 public interface CompanyListable {
-    default List<Company> list(CompanyRepository companyRepository){
-        return companyRepository.findAll(SORT_BY_CREATED_AT_DESC);
+
+    default List<Company> list(
+            CompanyRepository companyRepository,
+            Integer pageNo,
+            Integer pageSize,
+            String sortBy,
+            String direction
+    )
+    {
+        Sort sort = Sort.by("created_id").descending();
+
+        if(StringUtils.equals(direction,"asc")) {
+            sort = Sort.by(sortBy).ascending();
+        } else if(StringUtils.equals(direction,"desc")) {
+            sort = Sort.by(sortBy).descending();
+        }
+
+        Pageable paging = PageRequest.of(pageNo, pageSize, sort);
+        Page<Company> pagedResult = companyRepository.findAll(paging);
+
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
