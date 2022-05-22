332bcbcaaf7c4a04e600e9b4bc3c8638c8edd778
OlegHolovko <wodan78@gmail.com>
2022-05-22 12:40:16 +0300

    Overload findAll with sorting

--- src/main/java/com/holovko/haircutservie/module/company/repository/CompanyRepository.java
+++ src/main/java/com/holovko/haircutservie/module/company/repository/CompanyRepository.java
@@ -1,9 +1,13 @@
 package com.holovko.haircutservie.module.company.repository;
 
 import com.holovko.haircutservie.domain.Company;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository  extends CrudRepository<Company, Long> {
+    List<Company> findAll(Sort sort);
 }
