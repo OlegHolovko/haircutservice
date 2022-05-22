66b0a75f7058cf21ed774a47100eff5fd85aec01
OlegHolovko <wodan78@gmail.com>
2022-05-22 23:05:52 +0300

    Change return type for pagination

--- src/main/java/com/holovko/haircutservie/module/company/repository/CompanyRepository.java
+++ src/main/java/com/holovko/haircutservie/module/company/repository/CompanyRepository.java
@@ -1,13 +1,12 @@
 package com.holovko.haircutservie.module.company.repository;
 
 import com.holovko.haircutservie.domain.Company;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CompanyRepository  extends CrudRepository<Company, Long> {
-    List<Company> findAll(Sort sort);
+    Page<Company> findAll(Pageable pageable);
 }
