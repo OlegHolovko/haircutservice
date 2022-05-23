d6b00d4b2d53d0865313758646853aa8adb778ef
OlegHolovko <wodan78@gmail.com>
2022-05-23 22:55:18 +0300

    Realize read method as default

--- src/main/java/com/holovko/haircutservie/module/company/service/action/CompanyReadable.java
+++ src/main/java/com/holovko/haircutservie/module/company/service/action/CompanyReadable.java
@@ -1,10 +1,14 @@
 package com.holovko.haircutservie.module.company.service.action;
 
 import com.holovko.haircutservie.domain.Company;
+import com.holovko.haircutservie.module.company.exception.CompanyNotFoundException;
+import com.holovko.haircutservie.module.company.repository.CompanyRepository;
 
 public interface CompanyReadable {
-    default Company read(Long companyId){
-
-        return null;
+    default Company read(
+            CompanyRepository companyRepository,
+            Long companyId
+    ) throws CompanyNotFoundException {
+        return companyRepository.findById(companyId).orElseThrow(CompanyNotFoundException::new);
     }
 }
